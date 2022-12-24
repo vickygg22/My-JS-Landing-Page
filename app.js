6d2eecfd175c2d10ddbebfdd47eb8af9f0065f00
Victoria <victoria1998castellar@hotmail.com>
2022-12-24 10:42:16 +0000

    final version

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("If you see me, it's linked")
+
+const form = document.querySelector("form");
+const handleSubmit = () => {
+    sendEmail();
+}
+form.addEventListener("submit", handleSubmit)
+const sendEmail = () => {
+    const input = document.querySelector("input");
+    alert("This is the user's email: "+input.value)
+}
